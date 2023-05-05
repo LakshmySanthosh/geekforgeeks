c0f64d6cf89ce90944f60cf910f101edc7052048
Lakshmy <121610033+LakshmySanthosh@users.noreply.github.com>
2023-05-05 13:32:34 +0530

    Solved

--- goodSubtrees.cpp
+++ goodSubtrees.cpp
@@ -1,3 +1,26 @@
 /*
-
+You are given a root node of a binary Tree and each node of the binary tree is assigned a value and 
+you are also given an integer k and you have to return the count of Good subtrees of this binary Tree . 
+Good subtree is a subtree such that the number of distinct values in this subtree is less than or equal to k.
+Note: Subtree of a node consists of that node and all nodes of the subtrees of left and right child  of that node  if they exist .
 */
+
+class Solution{
+public:
+    int dfs(Node*& root,int k,int &cntSubtrees){
+        if(!root) return 0;
+        int lMask = dfs(root->left,k,cntSubtrees);
+        int rMask = dfs(root->right,k,cntSubtrees);
+        int cMask = lMask | rMask | (1<<root->data);
+        int bitCnt = __builtin_popcount(cMask);
+        if(bitCnt <= k) cntSubtrees++;
+        return cMask;
+    }
+
+    int goodSubtrees(Node *root,int k){
+        if(!root) return 0;
+        int cntSubtrees = 0;
+        dfs(root,k,cntSubtrees);
+        return cntSubtrees;
+    }
+};
