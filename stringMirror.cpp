7d99c4d0358d89ee77c04f261f1936dea41e8170
Lakshmy <121610033+LakshmySanthosh@users.noreply.github.com>
2023-05-07 22:51:42 +0530

    Update stringMirror.cpp

--- stringMirror.cpp
+++ stringMirror.cpp
@@ -1,2 +1,29 @@
 /*
+
 */
+
+class Solution{
+public:
+    string stringMirror(string str){
+        string sol;
+        sol.push_back(str[0]);
+        if(str[0]==str[1])
+        {
+            for(int i=sol.size()-1; i>=0; i--)  
+            sol.push_back(sol[i]);
+            return sol;
+        }
+        for(int i=1;i<str.size();i++)
+        {
+            if(int(str[i])<=int(str[i-1]))
+            {
+                sol.push_back(str[i]);
+            }
+            else
+                break;
+        }
+        for(int i=sol.size()-1; i>=0; i--)  
+            sol.push_back(sol[i]);
+        return sol;
+    }
+};
