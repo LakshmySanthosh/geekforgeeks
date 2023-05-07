843c02a116c636bd41f47083aeafa3f4ddc1dce7
Lakshmy <121610033+LakshmySanthosh@users.noreply.github.com>
2023-05-07 22:52:54 +0530

    Solved

--- stringMirror.cpp
+++ stringMirror.cpp
@@ -1,5 +1,7 @@
 /*
-
+You are given a string str of length n. You want to choose a non-zero integer k (k<=n), such that you can perform the following operation:
+Take the prefix of the string of length k and append the reverse of it to itself.
+Your task is to find the lexicographically smallest string you can get.
 */
 
 class Solution{
