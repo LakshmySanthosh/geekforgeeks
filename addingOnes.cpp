7f3c0ffb803e5bfe06ad8fdd5997ab2e70596fc8
Lakshmy <121610033+LakshmySanthosh@users.noreply.github.com>
2023-06-15 23:49:14 +0530

    Solved

--- addingOnes.cpp
+++ addingOnes.cpp
@@ -1,5 +1,5 @@
 /*
-You start with an array A of size N. Initially all elements of the array A are zero. You will be given K positive integers. 
+You start with an array A of size N. Initially all elements of the array A are zero. You will be given K positive integers
 Let j be one of these integers, you have to add 1 to all A[i], where i ≥ j. Your task is to print the array A after all these K updates are done.
 Note: 1-based indexing is used everywhere in this question.
 */
